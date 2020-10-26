936d25e043775d5f73ae58e085439534aad7d50b
Michał <33964835+DonTheDevPL@users.noreply.github.com>
2020-10-26 16:54:17 +0100

    sortowanie rankingu

--- front-end/js/requests.js
+++ front-end/js/requests.js
@@ -32,4 +32,30 @@ function fetchRanking() {
         dane.user = dane.user + `</table>`;
         document.getElementById("tabela_holder").innerHTML = dane.user;
     });
+
+    setTimeout(function () {
+        sortTable();
+    }, 500);
+}
+function sortTable() {
+    var table, rows, switching, i, x, y, shouldSwitch;
+    table = document.getElementById("tabela");
+    switching = true;
+    while (switching) {
+        switching = false;
+        rows = table.rows;
+        for (i = 1; i < (rows.length - 1); i++) {
+            shouldSwitch = false;
+            x = rows[i].getElementsByTagName("TD")[1];
+            y = rows[i + 1].getElementsByTagName("TD")[1];
+            if (Number(x.innerHTML) < Number(y.innerHTML)) {
+                shouldSwitch = true;
+                break;
+            }
+        }
+        if (shouldSwitch) {
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+        }
+    }
 }
